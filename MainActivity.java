d306635bba876581f3d04a5344db032f7c19a20b
My <git remote add origin https://github.com/zjun615/GitHubTest.git>
2017-10-31 14:48:11 +0800

    qwe

--- MainActivity.java
+++ MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //git连接
+        //123
     }
 }
